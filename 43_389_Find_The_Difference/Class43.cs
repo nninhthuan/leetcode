084aa4da83a2f19aeadfdbda030372e7a6f4dbb1
Administrator <nguyenninhthuan00021102@gmail.com>
2022-12-25 21:00:47 +0700

    134 19 21 43 57 72 7

---
+++ 43_389_Find_The_Difference/Class43.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43_389_Find_The_Difference
+{
+    public class Solution
+    {
+        public static char FindTheDifference(string s, string t)
+        {
+            int n = s.Length;
+            char[] arr1 = s.ToCharArray();
+            char[] arr2 = t.ToCharArray();
+            Array.Sort(arr1);
+            Array.Sort(arr2);
+            for (int i = 0; i < n; i++)
+                if (arr1[i] != arr2[i])
+                    return arr2[i];
+            return arr2[n];
+        }
+
+        public static void Main(string[] args)
+        {
+            string s = "abcd", t = "abcde";
+            char result = FindTheDifference(s ,t);
+        }
+    }
+}
---
+++ 7_561_Array_Pair_Sum/Class7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_561_Array_Pair_Sum
+{
+    public class Solution
+    {
+        public static int ArrayPairSum(int[] nums)
+        {
+            int sum = 0;
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - 1; i += 2)
+            {
+                sum += nums[i];
+            }
+
+            return sum;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 4, 3, 2 };
+            int result = ArrayPairSum(nums);
+        }
+    }
+}
