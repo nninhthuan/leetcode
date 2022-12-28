a21bab8cf14c2c6180aabad7ac90bb2ac2eb6c52
Administrator <nguyenninhthuan00021102@gmail.com>
2022-12-28 22:50:40 +0700

    25 29 34 44 71

---
+++ 44_371_Get_Sum/Class44.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _44_371_Get_Sum
+{
+    public class Solution
+    {
+        public static int GetSum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static void Main(string[] args)
+        {
+
+            int result = GetSum(1, 2);
+        }
+    }
+}
---
+++ 71_217_Contains_Duplicate/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        return nums.Distinct().Count() != nums.Length;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4 };
+
+        int[] arr2 = { 9, 5, 1, 2 };
+
+        bool result = ContainsDuplicate(arr2);
+    }
+}
