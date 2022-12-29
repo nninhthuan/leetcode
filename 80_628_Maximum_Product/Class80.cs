8d105ad0c3ad59e624c1e6e627b15ec76272fdd3
Administrator <nguyenninhthuan00021102@gmail.com>
2022-12-29 19:51:22 +0700

    .

---
+++ 80_628_Maximum_Product/Class80.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _80_628_Maximum_Product
+{
+    public class Solution
+    {
+        public static int MaximumProduct(int[] nums)
+        {
+            Array.Sort(nums);
+
+            int len = nums.Length;
+            int startMul = nums[0] * nums[1];
+            int endMul = nums[len - 2] * nums[len - 3];
+
+            if (nums[len - 1] > 0)
+            {
+                return nums[len - 1] * (startMul > endMul ? startMul : endMul);
+            }
+            else
+            {
+                return nums[len - 1] * endMul;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] grid = new int[] { 4, 2, 3 };
+            MaximumProduct(grid);
+        }
+    }
+}
---
+++ 83_268_Missing_Number/Class83.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _83_268_Missing_Number
+{
+    public class Solution
+    {
+        public static int MissingNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            bool b = false;
+            int i = 0;
+            if (nums.Length > 1)
+            {
+                for (i = 0; i < nums.Length - 1; i++)
+                {
+                    if (nums[0] == 0)
+                    {
+                        if (nums[i + 1] - nums[i] > 1)
+                        {
+                            b = true;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+
+                if (b)
+                {
+                    return nums[i] + 1;
+                }
+                else
+                {
+                    return nums[i] + 1;
+                }
+            }
+            else
+            {
+                if (nums[0] == 1)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return nums[0] + 1;
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] grid = new int[] { 4, 2, 3 };
+            MissingNumber(grid);
+        }
+    }
+}
