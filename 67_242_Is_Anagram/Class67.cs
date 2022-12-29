fb45ddf19637a470e31b11baa550a628392d26a3
Administrator <nguyenninhthuan00021102@gmail.com>
2022-12-29 19:32:29 +0700

    47 67 77

---
+++ 67_242_Is_Anagram/Class67.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _67_242_Is_Anagram
+{
+    public class Solution
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+
+            return s.OrderBy(c => c).SequenceEqual(t.OrderBy(x => x));
+
+        }
+
+        public static void Main(string[] args)
+        {
+            IsAnagram("anagram", "nagaram");
+        }
+    }
+}
